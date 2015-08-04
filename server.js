0a2fbd8d472fa9de79516adb7fedcbb2b7ef3b6b
Josh Phillips <jephillips@cohesionforce.com>
2015-08-04 15:32:51 -0700

    Removed client side html

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-/**
- * Created by Joshua on 8/4/2015.
- */
-var express = require('express');
-var app = express();
+///**
+// * Created by Joshua on 8/4/2015.
+// */
+//
+//var express = require('express');
+//var app = express();
+//
+//app.use('/client/', express.static(__dirname + '/client'));
+//
+//app.get('/hello', function(req,res){
+//    res.sendFile(__dirname+ '/client/index.html')
+//});
+//
+//var port = Number(process.env.PORT || 3000);
+//
+//app.listen(port, function() {
+//    console.log("Server listening on port " + port)
+//});
 
-app.use('/client/', express.static(__dirname + '/client'));
+var http = require('http');
 
-app.get('/hello', function(req,res){
-    res.sendFile(__dirname+ '/client/index.html')
-});
-
-var port = Number(process.env.PORT || 3000);
-
-app.listen(port, function() {
-    console.log("Server listening on port " + port)
+var server = http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello JsTogether</h1>');
 });
