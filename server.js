0bf76f7c32cf65f1ff8aa9d657e2b9b737a82d8a
Josh Phillips <jephillips@cohesionforce.com>
2015-08-04 15:51:06 -0700

    Moved / get to render method

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-///**
-// * Created by Joshua on 8/4/2015.
-// */
-//
-//var express = require('express');
-//var app = express();
-//
-//app.use('/client/', express.static(__dirname + '/client'));
-//
-//app.get('/hello', function(req,res){
-//    res.sendFile(__dirname+ '/client/index.html')
-//});
-//
-//var port = Number(process.env.PORT || 3000);
-//
-//app.listen(port, function() {
-//    console.log("Server listening on port " + port)
-//});
+/**
+ * Created by Joshua on 8/4/2015.
+ */
 
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello JsTogether</h1>');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/client'));
+
+app.get('/', function(request, response) {
+    response.render('/client/index.html');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
