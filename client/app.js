010c2fdd9156364fbcb1ddcf0ba2cedd81ba7641
Josh Phillips <jephillips@cohesionforce.com>
2015-08-12 16:09:14 -0500

    Moved over to Angular

--- client/app.js
+++ client/app.js
@@ -2,9 +2,26 @@
  * Created by Joshua on 8/11/2015.
  */
 
-class Application {
-    render() {
-        return <div> Hello{this.props.name} </div>
-    }
-}
+
+var AppCtrl = function (){
+    this.name = "AppCtrl"
+};
+
+
+angular.module('app', [
+    'ui.router',
+    'app.home'
+    ])
+    .controller('AppCtrl', AppCtrl)
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state("home", {
+                url: "/home",
+                templateUrl: "components/home/home.html"
+            });
+
+        $urlRouterProvider.otherwise("/home");
+    });
+
 
